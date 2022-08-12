6c7c1da00d4065f5a8a0c77d90cb1ea3bf62b9bf
Ernest Leung <ernestleung52@gmail.com>
2022-08-12 15:51:39 -0700

    wired up cli & local-api

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
+const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
+const local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
     // [] not require by command
     .command('serve [filename]')
@@ -9,5 +14,10 @@ exports.serveCommand = new commander_1.Command()
     // <> required by commander
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+    // cwd return where the user is typing the command from, dirname return the folder it in
+    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    // basename gives the file name
+    const fileName = path_1.default.basename(filename);
+    // Sending options info from CLI to local-api
+    (0, local_api_1.serve)(parseInt(options.port), fileName, dir);
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,4 +1,6 @@
+import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
 	// [] not require by command
@@ -6,6 +8,11 @@ export const serveCommand = new Command()
 	.description('Open a file for editing')
 	// <> required by commander
 	.option('-p, --port <number>', 'port to run server on', '4005')
-	.action((filename = 'notebook.js', options) => {
-		console.log(filename, options);
+	.action((filename = 'notebook.js', options: { port: string }) => {
+		// cwd return where the user is typing the command from, dirname return the folder it in
+		const dir = path.join(process.cwd(), path.dirname(filename));
+		// basename gives the file name
+		const fileName = path.basename(filename);
+		// Sending options info from CLI to local-api
+		serve(parseInt(options.port), fileName, dir);
 	});
