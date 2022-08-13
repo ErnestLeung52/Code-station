3e7369c418df2806638a8d3b042ef48d651e97fd
Ernest Leung <ernestleung52@gmail.com>
2022-08-13 00:43:09 -0700

    added fetch cells for redux

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from 'redux';
+import axios from 'axios';
 import { ActionType } from '../action-types';
 import {
 	Direction,
@@ -8,7 +9,7 @@ import {
 	InsertCellAfterAction,
 	Action,
 } from '../actions';
-import { CellTypes } from '../cell';
+import { Cell, CellTypes } from '../cell';
 import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -71,5 +72,25 @@ export const createBundle = (cellId: string, input: string) => {
 				bundle: result,
 			},
 		});
+	};
+};
+
+export const fetchCells = () => {
+	return async (dispatch: Dispatch<Action>) => {
+		dispatch({ type: ActionType.FETCH_CELLS });
+
+		try {
+			const { data }: { data: Cell[] } = await axios.get('/cells');
+
+			dispatch({
+				type: ActionType.FETCH_CELLS_COMPLETE,
+				payload: data,
+			});
+		} catch (error: any) {
+			dispatch({
+				type: ActionType.FETCH_CELLS_ERROR,
+				payload: error.message,
+			});
+		}
 	};
 };
